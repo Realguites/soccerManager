640dc7c67dc2fa69518fd7bac69db620a9d30dc5
root <root@localhost.localdomain>
2021-05-26 19:24:59 -0300

    Alternado porta

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -5,7 +5,9 @@ const knex = require('knex')({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
-    database : process.env.DB_NAME
+    database : process.env.DB_NAME,
+    port : process.env.DB_PORT
+    
   }
 });
 
